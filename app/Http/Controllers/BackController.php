3b0dca85648b377b2fcc67ae415536bed38b371a
francis65000 <franciscomanuel.gutierrez@iesjandula.es>
2024-09-21 18:02:00 +0200

    Instalacion Interfaz

---
+++ app/Http/Controllers/BackController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BackController extends Controller
+{
+    //INICIO BACK
+    public function verHome(Request $request)
+    {
+        //PARTE PARA LAS ESTADISTICAS
+        
+
+        //PASAMOS LOS DATOS A LA VISTA
+        
+
+        //PASAR ELEMENTOS PARA EL SLIDER
+        
+
+        return view('backend.home');
+    }
+}
---
+++ resources/views/comunes/masterBackend.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>DPTO Audiovisuales | IES Jándula</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+
+    @include('comunes.barraSuperior')
+
+    <div id="layoutSidenav">
+        @include('comunes.menuLateral')
+        <div id="layoutSidenav_content">
+
+            <!--A PARTIR DE AQUI EL CONTENIDO DE LA PAGINA-->
+            @yield('content')
+        
+            <!--A PARTIR DE AQUI EL FOOTER-->
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; DPTO Audiovisuales 2024</div>
+                        <div>
+                            <a href="#">Desarrollo Francisco Manuel Gutiérrez Carmona</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+        crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+</body>
+<script>
+    window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+});
+</script>
+
+</html>
